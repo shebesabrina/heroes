6c792f26bf9d288023a7e6114dd495aca9d3b700
Sabrina Robinson <sabrinarob@gmail.com>
2019-02-05 21:58:28 -0700

    Implements a service and get call for all heroes

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -9,14 +9,19 @@ import { HEROES } from '../mock-heroes';
 })
 
 export class HeroesComponent implements OnInit {
-
-  heroes = HEROES;
   selectedHero: Hero;
 
-  constructor() { }
+  heroes: Hero[];
+
+  constructor(private heroService: HeroService) { }
 
   ngOnInit() {
+    this.getHeroes();
   }
+
+  getHeroes(): void {
+  this.heroes = this.heroService.getHeroes();
+}
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
