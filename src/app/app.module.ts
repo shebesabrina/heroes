80b2828b51d7287f65fc6edec1b922f654e3f52e
Sabrina Robinson <sabrinarob@gmail.com>
2019-02-05 22:23:07 -0700

    Implements styling and service for messages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
-
-import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { MessagesComponent } from './messages/messages.component';
-
 
 @NgModule({
   declarations: [
@@ -18,12 +15,11 @@ import { MessagesComponent } from './messages/messages.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
     FormsModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+    // no need to place any providers due to the `providedIn` flag...
+  ],
+  bootstrap: [ AppComponent ]
 })
-export class AppModule {
-
- }
+export class AppModule { }
